baccb9ceb305512c98c9fc2d2f0c021f35b64879
felpfsf <feferreira.sf@gmail.com>
2022-09-14 19:10:00 -0300

    refac: main title gradient

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
       backgroundImage: {
         bgImageGalaxy: "url('/assets/background-galaxy.png')",
         nlw_gradient:
-          'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
+          'linear-gradient(89.86deg, #9572FC 5%, #43E7AD 70%, #E1D55D 95%)',
         gamecard_gradient:
           'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
       },
